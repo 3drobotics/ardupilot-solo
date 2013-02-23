ArduPilot/ardupilot

Copter: update firmware version to 2.9.1-rc1
Release Notes updated

ArduCopter/ArduCopter.pde
@@ -1,6 +1,6 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#define THISFIRMWARE "ArduCopter V2.9"
+#define THISFIRMWARE "ArduCopter V2.9.1-rc1"
 /*
  *  ArduCopter Version 2.9
  *  Lead author:	Jason Short

3drobotics/ardupilot-solo

ArduCopter/ArduCopter.pde
@@ -1,6 +1,6 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#define THISFIRMWARE "ArduCopter V2.9-dev"
+#define THISFIRMWARE "ArduCopter V2.9.1-dev"
 /*
  *  ArduCopter Version 2.9
  *  Lead author:	Jason Short