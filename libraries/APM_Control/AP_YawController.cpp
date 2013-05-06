ArduPilot/ardupilot

APM_Control: ROLL and PITCH controllers
These changes reduce height variation in turns and improve
robustness. the specific changes are:

1) Linked roll and pitch integrator protection to the final output
value so that if final output is on upper limit, the integrator is
prevented from increasing and vice-versa. This improves wind-up
protection.

2) Modified rate feedback in roll and pitch controllers to use body
rates rather than Euler or earth rates.

3) Changed the roll to pitch compensation to use measured roll angle
and estimated airspeed to calculate the component of turn rate
(assuming a level coordinated turn) around the pitch axis. This a
mathematically correct calculation and will work over a range of bank
angles and aircraft with minimal (if any) tuning required.

4) The integrator in the roll and pitch loop is clamped when the
estimated speed is below the minimum FBW speed

5) The noise filter in the pitch and roll loop has been changed to use
a FOH discretisation. This gives improved noise rejection and less
phase loss when compared to the previous filter that used a ZOH or
equivalent discretisation.

This has been flown on the rascal in the SITL and on a X-8 with
limited flight testing. Initial results have been encouraging with
reduced height variation in turns.  Compare to standard PIDS, the
revised pitch and roll controllers allow the use of rate feedback
(effectively the same as the old D term) without beating the servos to
death.  The bank angle compensation in the pitch loop works
effectively over a much larger range of bank angles and requires
minimal tuning compared to the old calculation.

YAW CONTROLLER

Currently testing the a 3-loop acceleration autopilot topology for the
yaw loop with feed forward yaw rate for turn compensation. This 3-loop
topology is commonly used in tactical skid to to turn missiles and is
easy to tune. The following block diagram shows the general signal
flow

Note that the acceleration measurement has to pass through 

libraries/APM_Control/AP_YawController.cpp
@@ -1,6 +1,8 @@
 // -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
 
 //	Code by Jon Challinger
+//  Modified by Paul Riseborough to implement a three loop autopilot
+//  topology
 //
 //	This library is free software; you can redistribute it and / or
 //	modify it under the terms of the GNU Lesser General Public
@@ -14,18 +16,14 @@
 extern const AP_HAL::HAL& hal;
 
 const AP_Param::GroupInfo AP_YawController::var_info[] PROGMEM = {
-	AP_GROUPINFO("P",    0, AP_YawController, _kp,       0),
-	AP_GROUPINFO("I",    1, AP_YawController, _ki,       0),
-	AP_GROUPINFO("IMAX", 2, AP_YawController, _imax,     0),
+	AP_GROUPINFO("K_A",    0, AP_YawController, _K_A,       0),
+	AP_GROUPINFO("K_I",    1, AP_YawController, _K_I,       0),
+	AP_GROUPINFO("K_D",    2, AP_YawController, _K_D,     0),
+	AP_GROUPINFO("K_RLL",   3, AP_YawController, _K_FF,   1),
 	AP_GROUPEND
 };
 
-// Low pass filter cut frequency for derivative calculation.
-// FCUT macro computes a frequency cut based on an acceptable delay.
-#define FCUT(d) (1 / ( 2 * 3.14f * (d) ) )
-const float AP_YawController::_fCut = FCUT(0.5f);
-
-int32_t AP_YawController::get_servo_out(float scaler, bool stick_movement)
+int32_t AP_YawController::get_servo_out(float scaler, bool stabilize, int16_t aspd_min, int16_t aspd_max)
 {
 	uint32_t tnow = hal.scheduler->millis();
 	uint32_t dt = tnow - _last_t;
@@ -40,43 +38,82 @@ int32_t AP_YawController::get_servo_out(float scaler, bool stick_movement)
 	
 	float delta_time = (float) dt / 1000.0f;
 	
-	if(stick_movement) {
-		if(!_stick_movement) {
-			_stick_movement_begin = tnow;
-		} else {
-			if(_stick_movement_begin < tnow-333) {
-				_freeze_start_time = tnow;
-			}
-		}
+	// Calculate yaw rate required to keep up with a constant height coordinated turn
+	float aspeed;
+	float rate_offset;
+	float bank_angle = _ahrs->roll;
+	// limit bank angle between +- 80 deg if right way up
+	if (fabsf(bank_angle) < 1.5707964f)	{
+	    bank_angle = constrain(bank_angle,-1.3962634,1.3962634f);
 	}
-	_stick_movement = stick_movement;
+	if (!_ahrs->airspeed_estimate(&aspeed)) {
+	    // If no airspeed available use average of min and max
+        aspeed = 0.5f*(float(aspd_min) + float(aspd_max));
+	}
+	rate_offset = (9.807f / constrain(aspeed , float(aspd_min), float(aspd_max))) * tanf(bank_angle) * cosf(bank_angle) * _K_FF;
 
-	Vector3f accels = _ins->get_accel();
+    // Get body rate vector (radians/sec)
+	Vector3f omega = _ahrs->get_gyro();
 	
-	// I didn't pull 512 out of a hat - it is a (very) loose approximation of
-    // 100*ToDeg(asinf(-accels.y/9.81f))
-	// which, with a P of 1.0, would mean that your rudder angle would be
-    // equal to your roll angle when
-	// the plane is still. Thus we have an (approximate) unit to go by.
-	float error = 512 * -accels.y;
+	// Apply a first order lowpass filter with a 20Hz cut-off
+	// Coefficients derived using a first order hold discretisation method
+	// Use of FOH discretisation increases high frequency noise rejection 
+	// and reduces phase loss compared to other methods
+	float rate = 0.0810026f * _last_rate_out + 0.6343426f * omega.z + 0.2846549f * _last_rate_in;
+	_last_rate_out = rate;
+	_last_rate_in = omega.z;
 	
-	// strongly filter the error
-	float RC = 1/(2*PI*_fCut);
-	error = _last_error +
-	(delta_time / (RC + delta_time)) * (error - _last_error);
-	_last_error = error;
-	// integrator
-	if(_freeze_start_time < (tnow - 2000)) {
-		if ((fabsf(_ki) > 0) && (dt > 0)) {
-			_integrator += (error * _ki) * scaler * delta_time;
-			if (_integrator < -_imax) _integrator = -_imax;
-			else if (_integrator > _imax) _integrator = _imax;
+	// Get the accln vector (m/s^2)
+	Vector3f accel = _ins->get_accel();
+
+	// Subtract the steady turn component of rate from the measured rate
+	// to calculate the rate relative to the turn requirement in degrees/sec
+	float rate_hp_in = ToDeg(rate - rate_offset);
+	
+	// Apply a high-pass filter to the rate to washout any steady state error
+	// due to bias errors in rate_offset
+	// Use a cut-off frequency of omega = 0.2 rad/sec
+	// Could make this adjustable by replacing 0.9960080 with (1 - omega * dt)
+	float rate_hp_out = 0.9960080f * _last_rate_hp_out + rate_hp_in - _last_rate_hp_in;
+	_last_rate_hp_out = rate_hp_out;
+	_last_rate_hp_in = rate_hp_in;
+
+	//Calculate input to integrator
+	float integ_in = - _K_I * (_K_A * accel.y + rate_hp_out);
+	
+	// Apply integrator, but clamp input to prevent control saturation and freeze integrator below min FBW speed
+	// Don't integrate if in stabilise mode as the integrator will wind up against the pilots inputs
+	// Don't integrate if _K_D is zero as integrator will keep winding up
+	if (!stabilize && _K_D > 0) {
+		//only integrate if airspeed above min value
+		if (aspeed > float(aspd_min))
+		{
+			// prevent the integrator from increasing if surface defln demand is above the upper limit
+			if (_last_out < -45) _integrator += max(integ_in * delta_time , 0);
+			// prevent the integrator from decreasing if surface defln demand  is below the lower limit
+			else if (_last_out > 45) _integrator += min(integ_in * delta_time , 0);
+			else _integrator += integ_in * delta_time;
 		}
 	} else {
 		_integrator = 0;
 	}
 	
-	return (error * _kp * scaler) + _integrator;
+	// Protect against increases to _K_D during in-flight tuning from creating large control transients
+	// due to stored integrator values
+	if (_K_D > _K_D_last && _K_D > 0) {
+	    _integrator = _K_D_last/_K_D * _integrator;
+	}
+	_K_D_last = _K_D;
+	
+	// Calculate demanded rudder deflection, +Ve deflection yaws nose right
+	// Save to last value before application of limiter so that integrator limiting
+	// can detect exceedance next frame
+	// Scale using inverse dynamic pressure (1/V^2)
+	_last_out =  _K_D * (_integrator - rate_hp_out) * scaler * scaler;
+
+	// Convert to centi-degrees and constrain
+	float out = constrain(_last_out * 100, -4500, 4500);
+	return out;
 }
 
 void AP_YawController::reset_I()

3drobotics/ardupilot-solo

libraries/APM_Control/AP_YawController.cpp
@@ -1,6 +1,8 @@
 // -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
 
 //	Code by Jon Challinger
+//  Modified by Paul Riseborough to implement a three loop autopilot
+//  topology
 //
 //	This library is free software; you can redistribute it and / or
 //	modify it under the terms of the GNU Lesser General Public
@@ -14,18 +16,14 @@
 extern const AP_HAL::HAL& hal;
 
 const AP_Param::GroupInfo AP_YawController::var_info[] PROGMEM = {
-	AP_GROUPINFO("P",    0, AP_YawController, _kp,       0),
-	AP_GROUPINFO("I",    1, AP_YawController, _ki,       0),
-	AP_GROUPINFO("IMAX", 2, AP_YawController, _imax,     0),
+	AP_GROUPINFO("K_A",    0, AP_YawController, _K_A,       0),
+	AP_GROUPINFO("K_I",    1, AP_YawController, _K_I,       0),
+	AP_GROUPINFO("K_D",    2, AP_YawController, _K_D,     0),
+	AP_GROUPINFO("K_RLL",   3, AP_YawController, _K_FF,   1),
 	AP_GROUPEND
 };
 
-// Low pass filter cut frequency for derivative calculation.
-// FCUT macro computes a frequency cut based on an acceptable delay.
-#define FCUT(d) (1 / ( 2 * 3.14f * (d) ) )
-const float AP_YawController::_fCut = FCUT(0.5f);
-
-int32_t AP_YawController::get_servo_out(float scaler, bool stick_movement)
+int32_t AP_YawController::get_servo_out(float scaler, bool stabilize, int16_t aspd_min, int16_t aspd_max)
 {
 	uint32_t tnow = hal.scheduler->millis();
 	uint32_t dt = tnow - _last_t;
@@ -40,23 +38,37 @@ int32_t AP_YawController::get_servo_out(float scaler, bool stick_movement)
 	
 	float delta_time = (float) dt / 1000.0f;
 	
-	if(stick_movement) {
-		if(!_stick_movement) {
-			_stick_movement_begin = tnow;
-		} else {
-			if(_stick_movement_begin < tnow-333) {
-				_freeze_start_time = tnow;
-			}
-		}
+	// Calculate yaw rate required to keep up with a constant height coordinated turn
+	float aspeed;
+	float rate_offset;
+	float bank_angle = _ahrs->roll;
+	// limit bank angle between +- 80 deg if right way up
+	if (fabsf(bank_angle) < 1.5707964f)	{
+	    bank_angle = constrain(bank_angle,-1.3962634,1.3962634f);
 	}
-    rate_offset = (9.807f / max(aspeed , float(aspd_min))) * tanf(bank_angle) * cosf(bank_angle) * _K_FF;
+	if (!_ahrs->airspeed_estimate(&aspeed)) {
+	    // If no airspeed available use average of min and max
+        aspeed = 0.5f*(float(aspd_min) + float(aspd_max));
+	}
+	rate_offset = (9.807f / constrain(aspeed , float(aspd_min), float(aspd_max))) * tanf(bank_angle) * cosf(bank_angle) * _K_FF;
 
     // Get body rate vector (radians/sec)
-	float omega_z = _ahrs->get_gyro().z;
+	float omega_z = _ahrs->get_gyro();
 	
+	// Apply a first order lowpass filter with a 20Hz cut-off
+	// Coefficients derived using a first order hold discretisation method
+	// Use of FOH discretisation increases high frequency noise rejection 
+	// and reduces phase loss compared to other methods
+	float rate = 0.0810026f * _last_rate_out + 0.6343426f * omega_z + 0.2846549f * _last_rate_in;
+	_last_rate_out = rate;
+	_last_rate_in = omega_z;
+	
+	// Get the accln vector (m/s^2)
+	float accel_y = _ins->get_accel().y;
+
 	// Subtract the steady turn component of rate from the measured rate
 	// to calculate the rate relative to the turn requirement in degrees/sec
-	float rate_hp_in = ToDeg(omega_z - rate_offset);
+	float rate_hp_in = ToDeg(rate - rate_offset);
 	
 	// Apply a high-pass filter to the rate to washout any steady state error
 	// due to bias errors in rate_offset
@@ -66,29 +78,42 @@ int32_t AP_YawController::get_servo_out(float scaler, bool stick_movement)
 	_last_rate_hp_out = rate_hp_out;
 	_last_rate_hp_in = rate_hp_in;
 
-	// Get the accln vector (m/s^2)
-	Vector3f accel = _ins->get_accel();
-
-	// Calculate input to integrator
-	float integ_in = - _K_I * (_K_A * accel.y + rate_hp_out);
+	//Calculate input to integrator
+	float integ_in = - _K_I * (_K_A * accel_y + rate_hp_out);
 	
-	// strongly filter the error
-	float RC = 1/(2*PI*_fCut);
-	error = _last_error +
-	(delta_time / (RC + delta_time)) * (error - _last_error);
-	_last_error = error;
-	// integrator
-	if(_freeze_start_time < (tnow - 2000)) {
-		if ((fabsf(_ki) > 0) && (dt > 0)) {
-			_integrator += (error * _ki) * scaler * delta_time;
-			if (_integrator < -_imax) _integrator = -_imax;
-			else if (_integrator > _imax) _integrator = _imax;
+	// Apply integrator, but clamp input to prevent control saturation and freeze integrator below min FBW speed
+	// Don't integrate if in stabilise mode as the integrator will wind up against the pilots inputs
+	// Don't integrate if _K_D is zero as integrator will keep winding up
+	if (!stabilize && _K_D > 0) {
+		//only integrate if airspeed above min value
+		if (aspeed > float(aspd_min))
+		{
+			// prevent the integrator from increasing if surface defln demand is above the upper limit
+			if (_last_out < -45) _integrator += max(integ_in * delta_time , 0);
+			// prevent the integrator from decreasing if surface defln demand  is below the lower limit
+			else if (_last_out > 45) _integrator += min(integ_in * delta_time , 0);
+			else _integrator += integ_in * delta_time;
 		}
 	} else {
 		_integrator = 0;
 	}
 	
-	return (error * _kp * scaler) + _integrator;
+	// Protect against increases to _K_D during in-flight tuning from creating large control transients
+	// due to stored integrator values
+	if (_K_D > _K_D_last && _K_D > 0) {
+	    _integrator = _K_D_last/_K_D * _integrator;
+	}
+	_K_D_last = _K_D;
+	
+	// Calculate demanded rudder deflection, +Ve deflection yaws nose right
+	// Save to last value before application of limiter so that integrator limiting
+	// can detect exceedance next frame
+	// Scale using inverse dynamic pressure (1/V^2)
+	_last_out =  _K_D * (_integrator - rate_hp_out) * scaler * scaler;
+
+	// Convert to centi-degrees and constrain
+	float out = constrain(_last_out * 100, -4500, 4500);
+	return out;
 }
 
 void AP_YawController::reset_I()