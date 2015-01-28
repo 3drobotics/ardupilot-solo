cb7bd3eabe277416288e5d269e87f98af4c2c766
Arthur Benemann <arthur.benemann@gmail.com>
2015-01-27 16:43:54 -0800

    AP_Gimbal: Creating a structure to hold gimbal state and mesuraments

--- libraries/AP_Gimbal/AP_Gimbal.cpp
+++ libraries/AP_Gimbal/AP_Gimbal.cpp
@@ -6,40 +6,65 @@
 #include <AP_Param.h>
 #include <AP_Gimbal.h>
 #include <GCS.h>
+#include <GCS_MAVLink.h>
 
 const AP_Param::GroupInfo AP_Gimbal::var_info[] PROGMEM = {
     AP_GROUPEND
 };
 
+uint16_t feedback_error_count;
 
-/// This one should be called periodically
-void AP_Gimbal::update_position()
+void AP_Gimbal::receive_feedback(mavlink_message_t *msg)
 {
-    ::printf("update position\n");
+    mavlink_msg_gimbal_feedback_decode(msg, &_state.measuraments);
+    update_state();
     send_control();
+}
+
+void AP_Gimbal::update_state()
+{
+    /*
+    _state.measuraments.accx; ///< X acceleration m/s/s
+    _state.measuraments.accy; ///< Y acceleration m/s/s
+    _state.measuraments.accz; ///< Z acceleration m/s/s
+    
+    _state.measuraments.gyrox; ///< Angular speed around X axis rad/s
+    _state.measuraments.gyroy; ///< Angular speed around Y axis rad/s
+    _state.measuraments.gyroz; ///< Angular speed around Z axis rad/s
+    _state.measuraments.joint_az; ///<  Azimuth joint angle (-10000,10000)
+    _state.measuraments.joint_roll; ///<  Roll joint angle (-10000,10000)
+    _state.measuraments.joint_el; ///<  Elevation joint angle (-10000,10000)
+
+    _ahrs.get_dcm_matrix();
+    */
+
+    // TODO add EKF code here
+
 }
 
 void AP_Gimbal::send_control()
 {
     mavlink_message_t msg;
-    mavlink_mount_control_t mount_control;
-    mount_control.input_a = _el_angle*100;
-    mount_control.input_b = _roll_angle*100;
-    mount_control.input_c = _az_angle*100;
-    mount_control.target_system = _sysid;
-    mount_control.target_component = _compid;
+    mavlink_gimbal_control_t control;
+    control.ratex = _state.target_rate[0];
+    control.ratey = _state.target_rate[1];
+    control.ratez = _state.target_rate[2];
+    control.target_system = _state.sysid;
+    control.target_component = _state.compid;
+    control.id = _state.measuraments.id;
 
+    mavlink_msg_gimbal_control_encode(1, 1, &msg, &control);
 
-    mount_control.input_a = 0;
-    mount_control.input_b = 0;
-    mount_control.input_c = 0;
-
-    mavlink_msg_mount_control_encode(1, 1, &msg, &mount_control);
     GCS_MAVLINK::routing.forward(&msg);
 }
 
-/// Return mount status information
+
 void AP_Gimbal::status_msg(mavlink_channel_t chan)
 {
-    mavlink_msg_mount_status_send(chan,0,0, _el_angle*100, _roll_angle*100, _az_angle*100);
+    mavlink_msg_mount_status_send(chan,0,0, _state.target_angles[EL]*100, _state.target_angles[ROLL]*100, _state.target_angles[AZ]*100);
+}
+
+/// This one should be called periodically
+void AP_Gimbal::update_position()
+{
 }
--- libraries/AP_Gimbal/AP_Gimbal.h
+++ libraries/AP_Gimbal/AP_Gimbal.h
@@ -20,24 +20,25 @@
 #include <AP_AHRS.h>
 #include <GCS_MAVLink.h>
 
+
 class AP_Gimbal
 {
 public:
     //Constructor
     AP_Gimbal(const struct Location *current_loc, const AP_AHRS &ahrs, uint8_t sysid, uint8_t compid) :
-        _ahrs(ahrs),
-        _roll_angle(0.0f),
-        _el_angle(0.0f),
-        _az_angle(0.0f),
-        _sysid(sysid),
-        _compid(compid)
+        _ahrs(ahrs)
     {
         AP_Param::setup_object_defaults(this, var_info);
         _current_loc = current_loc;
+        _state.sysid = sysid;
+        _state.compid = compid;
     }
+    
+   
         
     // MAVLink methods
     void                    status_msg(mavlink_channel_t chan);
+    void                    receive_feedback(mavlink_message_t *msg);
 
     // should be called periodically
     void                    update_position();
@@ -46,16 +47,25 @@ public:
     static const struct AP_Param::GroupInfo        var_info[];
 
 private:
-    const uint8_t                   _sysid,_compid;     // Target system id
 
+    enum GIMBAL_AXIS{
+        AZ,ROLL,EL
+    };
+
+    struct Gimbal_State {
+        uint8_t sysid;
+        uint8_t compid;
+        mavlink_gimbal_feedback_t measuraments;
+        float target_angles[3];     // degrees
+        float target_rate[3];       // degrees/s
+    };
+
+    struct Gimbal_State             _state;
     const AP_AHRS                   &_ahrs;             //  Rotation matrix from earth to plane.
     const struct Location           *_current_loc;
 
-    float                           _roll_angle; ///< degrees
-    float                           _el_angle; ///< degrees
-    float                           _az_angle;  ///< degrees
-
     void                    send_control();
+    void                    update_state();
 };
 
 #endif // __AP_MOUNT_H__
