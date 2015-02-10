bfb664783e2930d53db16c0fef63de479398637a
Arthur Benemann <arthur.benemann@gmail.com>
2015-02-10 11:26:37 -0800

    AP_Gimbal: Clean-up of library

--- libraries/AP_Gimbal/AP_Gimbal.cpp
+++ libraries/AP_Gimbal/AP_Gimbal.cpp
@@ -18,54 +18,46 @@ static float angRateLimit = 0.5f;
 
 void AP_Gimbal::receive_feedback(mavlink_message_t *msg)
 {
-    uint8_t expected_id = _state.measuraments.id +1;
-    mavlink_msg_gimbal_feedback_decode(msg, &_state.measuraments);
-    if(expected_id !=_state.measuraments.id){
-        feedback_error_count++;
-
-        ::printf("error count: %d\t %d\t %d\n", feedback_error_count,expected_id,_state.measuraments.id);
-    }
-
-
+    decode_feedback(msg);
     update_state();
     send_control();
 }
+    
 
+void AP_Gimbal::decode_feedback(mavlink_message_t *msg)
+{
+    mavlink_gimbal_feedback_t feedback_msg;
+    uint8_t expected_id = _measurament.id +1;
+    mavlink_msg_gimbal_feedback_decode(msg, &feedback_msg);
+    _measurament.id = feedback_msg.id;
+
+    if(expected_id !=_measurament.id){
+        feedback_error_count++;
+        ::printf("error count: %d\n", feedback_error_count);
+    }
+
+    _measurament.delta_angles.x = feedback_msg.gyrox;
+    _measurament.delta_angles.y = feedback_msg.gyroy,
+    _measurament.delta_angles.z = feedback_msg.gyroz;
+    _measurament.delta_velocity.x = feedback_msg.accx,
+    _measurament.delta_velocity.y = feedback_msg.accy,
+    _measurament.delta_velocity.z = feedback_msg.accz;   
+    _measurament.joint_angles.x = feedback_msg.joint_roll;
+    _measurament.joint_angles.y = feedback_msg.joint_el,
+    _measurament.joint_angles.z = feedback_msg.joint_az;
+}
 
 
 void AP_Gimbal::update_state()
 {
-    /*
-    _state.measuraments.accx; ///< X acceleration m/s/s
-    _state.measuraments.accy; ///< Y acceleration m/s/s
-    _state.measuraments.accz; ///< Z acceleration m/s/s
-    
-    _state.measuraments.gyrox; ///< Angular speed around X axis rad/s
-    _state.measuraments.gyroy; ///< Angular speed around Y axis rad/s
-    _state.measuraments.gyroz; ///< Angular speed around Z axis rad/s
-    _state.measuraments.joint_az; ///<  Azimuth joint angle (-10000,10000)
-    _state.measuraments.joint_roll; ///<  Roll joint angle (-10000,10000)
-    _state.measuraments.joint_el; ///<  Elevation joint angle (-10000,10000)
-
-    _ahrs.get_dcm_matrix();
-    */
-
-    // TODO add EKF code here
-
-
-    Vector3f gimbalRateDemVec;
-    Vector3f joint_angles(_state.measuraments.joint_roll,
-                          _state.measuraments.joint_el,
-                          _state.measuraments.joint_az);
-
-   // Define rotation from vehicle to gimbal using a 312 rotation sequence
+    // Define rotation from vehicle to gimbal using a 312 rotation sequence
     Matrix3f Tvg;
-    float cosPhi = cosf(joint_angles.x);
-    float cosTheta = cosf(joint_angles.y);
-    float sinPhi = sinf(joint_angles.x);
-    float sinTheta = sinf(joint_angles.y);
-    float sinPsi = sinf(joint_angles.z);
-    float cosPsi = cosf(joint_angles.z);
+    float cosPhi = cosf(_measurament.joint_angles.x);
+    float cosTheta = cosf(_measurament.joint_angles.y);
+    float sinPhi = sinf(_measurament.joint_angles.x);
+    float sinTheta = sinf(_measurament.joint_angles.y);
+    float sinPsi = sinf(_measurament.joint_angles.z);
+    float cosPsi = cosf(_measurament.joint_angles.z);
     Tvg[0][0] = cosTheta*cosPsi-sinPsi*sinPhi*sinTheta;
     Tvg[1][0] = -sinPsi*cosPhi;
     Tvg[2][0] = cosPsi*sinTheta+cosTheta*sinPsi*sinPhi;
@@ -93,7 +85,7 @@ void AP_Gimbal::update_state()
     // Zeroing the yaw demand in earth frame
     Vector3f deltaAngErrNED;
     deltaAngErrNED = Teg.transposed() * deltaAngErr;
-    deltaAngErrNED.z = -joint_angles.z * K_gimbalRate;
+    deltaAngErrNED.z = -_measurament.joint_angles.z * K_gimbalRate;
     deltaAngErr = Teg * deltaAngErrNED;
 
     // multiply the rotation vector by an error gain to calculate a demanded   vehicle frame relative rate vector
@@ -111,42 +103,21 @@ void AP_Gimbal::update_state()
     gimbalRateDemVec += forwardPathRateDem;
     */
 
-    _state.target_rate[X] = gimbalRateDemVec.x;
-    _state.target_rate[Y] = gimbalRateDemVec.y;
-    _state.target_rate[Z] = gimbalRateDemVec.z;
-
-    //::printf("joint \t%1.2f\t%1.2f\t%1.2f\t gyro \t%1.4f\t%1.4f\t%1.4f\t rate \t%1.2f\t%1.2f\t%1.2f\t\n",_state.measuraments.joint_roll,_state.measuraments.joint_el,_state.measuraments.joint_az,_state.measuraments.gyrox,_state.measuraments.gyroy,_state.measuraments.gyroz, _state.target_rate[X], _state.target_rate[Y], _state.target_rate[Z]);
+    //::printf("joint \t%1.2f\t%1.2f\t%1.2f\t gyro \t%1.4f\t%1.4f\t%1.4f\t rate \t%1.2f\t%1.2f\t%1.2f\t\n",_state.feedback_msg.joint_roll,_state.feedback_msg.joint_el,_state.feedback_msg.joint_az,_state.feedback_msg.gyrox,_state.feedback_msg.gyroy,_state.feedback_msg.gyroz, _state.target_rate[X], _state.target_rate[Y], _state.target_rate[Z]);
 }
 
 void AP_Gimbal::send_control()
 {
     mavlink_message_t msg;
     mavlink_gimbal_control_t control;
-    control.ratex = _state.target_rate[X];
-    control.ratey = _state.target_rate[Y];
-    control.ratez = _state.target_rate[Z];
-    /*
-    control.ratex = 0;
-    control.ratey = 0;
-    control.ratez = 0;
-    */
+    control.target_system = _sysid;
+    control.target_component = _compid;
+    control.id = _measurament.id;
 
-    control.target_system = _state.sysid;
-    control.target_component = _state.compid;
-    control.id = _state.measuraments.id;
+    control.ratex = gimbalRateDemVec.x;
+    control.ratey = gimbalRateDemVec.y;
+    control.ratez = gimbalRateDemVec.z;    
 
     mavlink_msg_gimbal_control_encode(1, 1, &msg, &control);
-
     GCS_MAVLINK::routing.forward(&msg);
-}
-
-
-void AP_Gimbal::status_msg(mavlink_channel_t chan)
-{
-    mavlink_msg_mount_status_send(chan,0,0, _state.target_angles[EL]*100, _state.target_angles[ROLL]*100, _state.target_angles[AZ]*100);
-}
-
-/// This one should be called periodically
-void AP_Gimbal::update_position()
-{
 }
