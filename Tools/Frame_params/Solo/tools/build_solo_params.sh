968325b91312f2ac1ce1a4252b7b445968c0fe03
Jonathan Challinger <mr.challinger@gmail.com>
2015-04-02 15:48:44 -0700

    Frame_params: change param build system to build only Solo.param

--- Tools/Frame_params/Solo/tools/build_solo_params.sh
+++ Tools/Frame_params/Solo/tools/build_solo_params.sh
@@ -2,11 +2,8 @@
 
 set -e
 set -x
-#setup common.param:
-tools/mavparmstrip.py --strip tools/paramstriplist base/defaults.param common.param
-tools/mavparmmerge.py common.param base/solo.param common.param
 
-tools/mavparmmerge.py common.param configs/minus.param Solo-minus.param
-tools/mavparmmerge.py common.param configs/tmprops.param Solo-TMprops.param
-tools/mavparmmerge.py common.param configs/apcprops.param Solo-APCprops.param
-tools/mavparmmerge.py Solo-TMprops.param configs/rccal.param Solo-TMprops-rccal.param
+tools/mavparmstrip.py --strip tools/paramstriplist base/defaults.param Solo.param
+tools/mavparmmerge.py Solo.param base/solo.param Solo.param
+tools/mavparmmerge.py Solo.param configs/tmprops.param Solo.param
+tools/mavparmmerge.py Solo.param configs/rccal.param Solo.param
