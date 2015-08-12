7badc03aada3ce95accf77d2fc920a5369dc512c
Paul Riseborough <p-riseborough@live.com.au>
2015-08-12 14:17:55 +1000

    AP_NavEKF: Prevent noisy baro readings from failing pre-flight check
    Wind gusts can cause baro fluctuations that are failing the pre-flight health check. This applies a filter to the innovation sequence to reduce the effect of zero mean noise.

--- libraries/AP_NavEKF/AP_NavEKF.h
+++ libraries/AP_NavEKF/AP_NavEKF.h
@@ -676,6 +676,7 @@ private:
     float gpsDriftNE;               // amount of drift detected in the GPS position during pre-flight GPs checks
     float gpsVertVelFilt;           // amount of filterred vertical GPS velocity detected durng pre-flight GPS checks
     float gpsHorizVelFilt;          // amount of filtered horizontal GPS velocity detected during pre-flight GPS checks
+    float hgtInnovFiltState;        // state used for fitering of the height innovations used for pre-flight checks
 
     // Used by smoothing of state corrections
     Vector10 gpsIncrStateDelta;    // vector of corrections to attitude, velocity and position to be applied over the period between the current and next GPS measurement
