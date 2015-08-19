d865e8af3a539088883cc6c715b220cd283993e2
Paul Riseborough <p-riseborough@live.com.au>
2015-08-19 12:32:09 +1000

    AP_NavEKF: Reset mag and heading states to try and pass pre-flight checks

--- libraries/AP_NavEKF/AP_NavEKF.h
+++ libraries/AP_NavEKF/AP_NavEKF.h
@@ -677,6 +677,8 @@ private:
     float gpsVertVelFilt;           // amount of filterred vertical GPS velocity detected durng pre-flight GPS checks
     float gpsHorizVelFilt;          // amount of filtered horizontal GPS velocity detected during pre-flight GPS checks
     float hgtInnovFiltState;        // state used for fitering of the height innovations used for pre-flight checks
+    uint32_t lastGpsMagPass_ms;     // last pass time in msec of the magnetometer innovation consistency check used to determine if the EKF can start using GPS
+    bool consistentMagData;         // true when the magnetometers are passing consistency checks
 
     // Used by smoothing of state corrections
     Vector10 gpsIncrStateDelta;    // vector of corrections to attitude, velocity and position to be applied over the period between the current and next GPS measurement
